a7403a08eda81fe8284b32a46eb79f72c12638e7
najwa karimah <karimahnajwa822@gmail.com>
2026-07-20 17:04:59 +0700

    fix: resolve merge conflict in urls.py

--- eventrent/urls.py
+++ eventrent/urls.py
@@ -3,9 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-
-=======
->>>>>>> e43f98125f5d66dea1767357b9d875f11f76b2c0
     path('', include('penyewaan.urls')),
 ]
