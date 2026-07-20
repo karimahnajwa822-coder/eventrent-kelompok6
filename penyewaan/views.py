e43f98125f5d66dea1767357b9d875f11f76b2c0
najwa karimah <karimahnajwa822@gmail.com>
2026-07-20 15:49:21 +0700

    Menambahkan login dan dashboard

--- penyewaan/views.py
+++ penyewaan/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        return redirect('dashboard')
+
+    return render(request, 'penyewaan/login.html')
+
+
+def register_view(request):
+    if request.method == "POST":
+        return redirect('login')
+
+    return render(request, 'penyewaan/register.html')
+
+
+def dashboard(request):
+    return render(request, 'penyewaan/dashboard.html')
